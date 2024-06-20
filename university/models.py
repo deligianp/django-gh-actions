e3e2fa76b07d096bca79e0227cf7d2344314f782
pandel1993 <pandel1993@gmail.com>
2024-06-20 20:33:47 +0300

    Implement CourseClass model

--- university/models.py
+++ university/models.py
@@ -4,3 +4,8 @@ from django.db import models
 # Create your models here.
 class Course(models.Model):
     name = models.CharField(max_length=255)
+
+
+class CourseClass(models.Model):
+    year = models.SmallIntegerField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
